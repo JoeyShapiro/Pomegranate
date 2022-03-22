99e161833325c4412cf1f8ce19fb07525e7b0db9
JoeyShapiro <joeyashapiro@gmail.com>
2022-03-22 11:45:14 -0500

    Initial Commit

---
+++ Pomegranate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomegranate
+//
+//  Created by Joey Shapiro on 3/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pomegranate/PomegranateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomegranateApp.swift
+//  Pomegranate
+//
+//  Created by Joey Shapiro on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct PomegranateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
